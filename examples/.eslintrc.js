3787523ebb47aa5f6f8c44ac687be26b4318ceca
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-11-05 23:35:40 +1100

    Update eslint setup for examples

---
+++ examples/.eslintrc.js
@@ -0,0 +1,15 @@
+const config = require('../.eslintrc');
+
+module.exports = {
+  ...config,
+  extends: [...config.extends, 'plugin:@next/next/recommended'],
+  rules: { ...config.rules, 'react/react-in-jsx-scope': 0, 'react/jsx-uses-react': 0 },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended']
+    }
+  ]
+};
