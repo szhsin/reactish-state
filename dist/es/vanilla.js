4526dcd9cea19fb00cffc72e9e160a6f2d9e0064
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-11-13 16:44:30 +1100

    Memoize selector to return a cached result when input stay the same

--- dist/es/vanilla.js
+++ dist/es/vanilla.js
@@ -25,6 +25,12 @@ var state = function state(initialValue, actionCreator) {
     actions: actionCreator && actionCreator(set, get)
   };
 };
+var isEqual = function isEqual(args1, args2) {
+  for (var i = 0; i < args1.length; i++) {
+    if (!Object.is(args1[i], args2[i])) return false;
+  }
+  return true;
+};
 var selector = function selector() {
   for (var _len = arguments.length, items = new Array(_len), _key = 0; _key < _len; _key++) {
     items[_key] = arguments[_key];
@@ -32,11 +38,19 @@ var selector = function selector() {
   var lastIndex = items.length - 1;
   var selectorFunc = items[lastIndex];
   items.length = lastIndex;
+  var cache;
   return {
     get: function get() {
-      return selectorFunc.apply(void 0, items.map(function (item) {
+      var args = items.map(function (item) {
         return item.get();
-      }));
+      });
+      if (cache && isEqual(args, cache.args)) return cache.ret;
+      var ret = selectorFunc.apply(void 0, args);
+      cache = {
+        args: args,
+        ret: ret
+      };
+      return ret;
     },
     subscribe: function subscribe(listener) {
       var unsubscribers = items.map(function (item) {
--- src/vanilla.ts
+++ src/vanilla.ts
@@ -50,15 +50,27 @@ type ReactishValueArray<R extends ReactishArray> = {
 };
 type SelectorFunc<R extends ReactishArray, T> = (...args: ReactishValueArray<R>) => T;
 
+const isEqual = (args1: unknown[], args2: unknown[]) => {
+  for (let i = 0; i < args1.length; i++) {
+    if (!Object.is(args1[i], args2[i])) return false;
+  }
+  return true;
+};
+
 const selector = <R extends ReactishArray, T>(...items: [...R, SelectorFunc<R, T>]) => {
   const lastIndex = items.length - 1;
   const selectorFunc = items[lastIndex] as SelectorFunc<R, T>;
   items.length = lastIndex;
+  let cache: { args: unknown[]; ret: T } | undefined;
+
   return {
-    get: () =>
-      selectorFunc(
-        ...((items as ReactishArray).map((item) => item.get()) as ReactishValueArray<R>)
-      ),
+    get: () => {
+      const args = (items as ReactishArray).map((item) => item.get()) as ReactishValueArray<R>;
+      if (cache && isEqual(args, cache.args)) return cache.ret;
+      const ret = selectorFunc(...args);
+      cache = { args, ret };
+      return ret;
+    },
     subscribe: (listener: Listener) => {
       const unsubscribers = (items as ReactishArray).map((item) => item.subscribe(listener));
       return () => unsubscribers.forEach((unsubscribe) => unsubscribe());
