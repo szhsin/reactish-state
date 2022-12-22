69378a2c0fccded17f40b407e76444832d6b5983
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-12-22 22:01:40 +1100

    Add rollup build for plugin

--- src/middleware/reduxDevtools.ts
+++ src/middleware/reduxDevtools.ts
@@ -4,9 +4,7 @@ import type { Middleware } from '../common';
 const reduxDevtools: Middleware = (set, get, config) => {
   if (typeof window === 'undefined' || !window.__REDUX_DEVTOOLS_EXTENSION__) return set;
 
-  const devtools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
-    name: config?.key
-  });
+  const devtools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({ name: config?.key });
   devtools.init(get());
 
   return function (value, action) {
