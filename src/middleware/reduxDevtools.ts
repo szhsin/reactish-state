1076402adc19f5fad7b5ceb237dc654ab3b7d73d
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-12-28 09:44:33 +1100

    Enable to merge state in reduxDevtools

--- src/middleware/reduxDevtools.ts
+++ src/middleware/reduxDevtools.ts
@@ -1,18 +1,38 @@
 import type {} from '@redux-devtools/extension';
 import type { Middleware } from '../common';
 
-const reduxDevtools: Middleware = ({ set, get }, config) => {
-  if (typeof window === 'undefined' || !window.__REDUX_DEVTOOLS_EXTENSION__) return set;
+type ReduxDevtools = (options?: { name?: string; merging?: boolean }) => Middleware;
 
-  const devtools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({ name: config?.key });
-  devtools.init(get());
+const reduxDevtools: ReduxDevtools = ({ name, merging } = {}) => {
+  let devtoolsExt: Window['__REDUX_DEVTOOLS_EXTENSION__'];
+  if (typeof window === 'undefined' || !(devtoolsExt = window.__REDUX_DEVTOOLS_EXTENSION__))
+    return ({ set }) => set;
 
-  return function (value, action) {
-    set.apply(null, arguments as unknown as Parameters<typeof set>);
-    devtools.send(
-      typeof action === 'string' ? { type: action } : action || { type: 'SET', value },
-      get()
-    );
+  let devtools: ReturnType<typeof devtoolsExt['connect']>;
+  if (merging) devtools = devtoolsExt.connect({ name });
+  const mergedState: { [index: string]: unknown } = {};
+
+  return ({ set, get }, config) => {
+    const key = config?.key;
+    if (merging) {
+      if (!key)
+        throw new Error(
+          '[reactish-state] state should be provided with a string `key` in the config object when the `reduxDevtools` middleware enables the `merging` option.'
+        );
+      mergedState[key] = get();
+      devtools.init(mergedState);
+    } else {
+      devtools = devtoolsExt!.connect({ name: key });
+      devtools.init(get());
+    }
+
+    return function (value, action) {
+      set.apply(null, arguments as unknown as Parameters<typeof set>);
+      devtools.send(
+        typeof action === 'string' ? { type: action } : action || { type: 'SET', value },
+        merging ? ((mergedState[key!] = get()), mergedState) : get()
+      );
+    };
   };
 };
 
