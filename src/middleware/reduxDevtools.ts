0ba269380fe98640cb25c7bed19adffb88ec937c
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-12-28 10:23:49 +1100

    Remove merging option to simplify reduxDevtools

--- src/middleware/reduxDevtools.ts
+++ src/middleware/reduxDevtools.ts
@@ -1,38 +1,31 @@
 import type {} from '@redux-devtools/extension';
 import type { Middleware } from '../common';
 
-type ReduxDevtools = (options?: { name?: string; merging?: boolean }) => Middleware;
+type ReduxDevtools = (options?: { name?: string }) => Middleware;
 
-const reduxDevtools: ReduxDevtools = ({ name, merging } = {}) => {
+const reduxDevtools: ReduxDevtools = ({ name } = {}) => {
   let devtoolsExt: Window['__REDUX_DEVTOOLS_EXTENSION__'];
   if (typeof window === 'undefined' || !(devtoolsExt = window.__REDUX_DEVTOOLS_EXTENSION__))
     return ({ set }) => set;
 
-  let devtools: ReturnType<typeof devtoolsExt['connect']>;
-  if (merging) devtools = devtoolsExt.connect({ name });
+  const devtools = devtoolsExt.connect({ name });
   const mergedState: { [index: string]: unknown } = {};
 
   return ({ set, get }, config) => {
     const key = config?.key;
-    if (merging) {
-      if (!key)
-        throw new Error(
-          '[reactish-state] state should be provided with a string `key` in the config object when the `reduxDevtools` middleware enables the `merging` option.'
-        );
-      mergedState[key] = get();
-      devtools.init(mergedState);
-    } else {
-      devtools = devtoolsExt!.connect({ name: key });
-      devtools.init(get());
-    }
+    if (!key)
+      throw new Error(
+        '[reactish-state] state should be provided with a string `key` in the config object when the `reduxDevtools` middleware is used.'
+      );
+    mergedState[key] = get();
+    devtools.init(mergedState);
 
     return function (value, action) {
       set.apply(null, arguments as unknown as Parameters<typeof set>);
+      mergedState[key] = get();
       devtools.send(
-        typeof action === 'string'
-          ? { type: action }
-          : action || { type: merging ? `SET_${key}` : 'SET', value },
-        merging ? ((mergedState[key!] = get()), mergedState) : get()
+        typeof action === 'string' ? { type: action } : action || { type: `SET_${key}`, value },
+        mergedState
       );
     };
   };
