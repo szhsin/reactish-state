97b8743429e5e7672a8cb09640f1875450acbb45
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-12-29 15:55:55 +1100

    Unit tests for `reduxDevtools` middleware

---
+++ src/__tests__/middleware/reduxDevtools.test.ts
@@ -0,0 +1,59 @@
+import type {} from '@redux-devtools/extension';
+import { createState } from '../../';
+import { reduxDevtools } from '../../middleware';
+
+describe('reduxDevtools', () => {
+  const init = jest.fn();
+  const send = jest.fn();
+  const connect = jest.fn().mockReturnValue({ init, send });
+
+  beforeAll(() => {
+    (global.window as {
+      __REDUX_DEVTOOLS_EXTENSION__?: {
+        connect: NonNullable<Window['__REDUX_DEVTOOLS_EXTENSION__']>['connect'];
+      };
+    }) = {
+      __REDUX_DEVTOOLS_EXTENSION__: { connect }
+    };
+  });
+
+  afterAll(() => {
+    delete global.window.__REDUX_DEVTOOLS_EXTENSION__;
+  });
+
+  test('should send actions', () => {
+    const state = createState({ middleware: reduxDevtools({ name: 'my-app' }) });
+    const price = state(0.99, null, { key: 'price' });
+    const quantity = state(1, null, { key: 'qty' });
+
+    expect(connect).toHaveBeenLastCalledWith({ name: 'my-app' });
+    expect(init).toHaveBeenLastCalledWith({ price: 0.99, qty: 1 });
+
+    quantity.set(2);
+    expect(send).toHaveBeenLastCalledWith({ type: 'SET_qty', value: 2 }, { price: 0.99, qty: 2 });
+
+    quantity.set((q) => q + 1, 'increase/qty');
+    expect(send).toHaveBeenLastCalledWith({ type: 'increase/qty' }, { price: 0.99, qty: 3 });
+
+    quantity.set((q) => q + 2, { type: 'increaseBy/qty', by: 2 });
+    expect(send).toHaveBeenLastCalledWith(
+      { type: 'increaseBy/qty', by: 2 },
+      { price: 0.99, qty: 5 }
+    );
+
+    expect(price.get()).toBe(0.99);
+    expect(quantity.get()).toBe(5);
+  });
+
+  test('should warn if a key is not provided in config', () => {
+    const state = createState({ middleware: reduxDevtools() });
+    expect(() => state('no key')).toThrow();
+    state('with key', null, { key: 'some-key' });
+  });
+});
+
+test('reduxDevtools should return original set when there is no redux devtools extension', () => {
+  const set = jest.fn();
+  // eslint-disable-next-line
+  expect(reduxDevtools()({ set } as any)).toBe(set);
+});
