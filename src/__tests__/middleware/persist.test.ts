b84d9ec397f087f651e81c5773ef130407371ee7
Zheng Song <41896553+szhsin@users.noreply.github.com>
2023-01-17 20:24:12 +1100

    Allow loading `undefined` from storage

--- src/__tests__/middleware/persist.test.ts
+++ src/__tests__/middleware/persist.test.ts
@@ -4,7 +4,9 @@ import { persist } from '../../middleware';
 test('persist should load and save data to storage', () => {
   const getItem = jest
     .fn<string | null, string[]>()
-    .mockImplementation((key) => (key === 'cat-colour' ? '"Calico"' : null));
+    .mockImplementation((key) =>
+      key === 'cat-colour' ? '"Calico"' : key === 'cat-age' ? 'undefined' : null
+    );
   const setItem = jest.fn();
   (global.localStorage as Pick<Storage, 'getItem' | 'setItem'>) = { getItem, setItem };
 
@@ -13,21 +15,26 @@ test('persist should load and save data to storage', () => {
 
   const colour = state('Ginger', null, { key: 'colour' });
   const age = state(1, null, { key: 'age' });
+  const sex = state('Female', null, { key: 'sex' });
   expect(colour.get()).toBe('Ginger');
   expect(age.get()).toBe(1);
+  expect(sex.get()).toBe('Female');
 
   middleware.hydrate();
   expect(colour.get()).toBe('Calico');
-  expect(age.get()).toBe(1);
+  expect(age.get()).toBeUndefined();
+  expect(sex.get()).toBe('Female');
   expect(getItem).toHaveBeenCalledWith('cat-colour');
   expect(getItem).toHaveBeenCalledWith('cat-age');
+  expect(getItem).toHaveBeenCalledWith('cat-sex');
 
   age.set(2);
   expect(colour.get()).toBe('Calico');
   expect(age.get()).toBe(2);
+  expect(sex.get()).toBe('Female');
   expect(setItem).toHaveBeenCalledWith('cat-age', '2');
 
-  expect(getItem).toHaveBeenCalledTimes(2);
+  expect(getItem).toHaveBeenCalledTimes(3);
   expect(setItem).toHaveBeenCalledTimes(1);
 });
 
