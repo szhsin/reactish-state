a6083bdc0d98c787d4ed1b4a4d2ac91dcd151d9c
Zheng Song <szhsin9@gmail.com>
2025-02-22 16:14:56 +1100

    Add documentation for shim setup (#41)

--- src/react/useSnapshot.ts
+++ src/react/useSnapshot.ts
@@ -3,7 +3,9 @@ import { useSyncExternalStore } from './shim';
 
 const useSnapshot = <T>({ subscribe, get }: Reactish<T>) => {
   if (process.env.NODE_ENV !== 'production' && !useSyncExternalStore) {
-    throw new Error('[reactish-state] Shim setup is required for React 16/17.');
+    throw new Error(
+      '[reactish-state] Shim setup is required for React 16/17. See: https://github.com/szhsin/reactish-state/tree/master?tab=readme-ov-file#react-1617-setup'
+    );
   }
   return useSyncExternalStore<T>(subscribe, get, get);
 };
