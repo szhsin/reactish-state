13a3ba1b98e56a1f282261d37a52584faf33eb0a
szhsin <41896553+szhsin@users.noreply.github.com>
2026-09-11 19:05:53 +0000

    chore(deps): update all dependencies

--- dist/esm/middleware/index.mjs
+++
@@ -1,4 +0,0 @@
-import { applyMiddleware } from "./applyMiddleware.mjs";
-import { persist } from "./persist.mjs";
-import { reduxDevtools } from "./reduxDevtools.mjs";
-export { applyMiddleware, persist, reduxDevtools };
