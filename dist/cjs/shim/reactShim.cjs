6838e22e0c98cab3290c233b94333c58ba126b5d
szhsin <41896553+szhsin@users.noreply.github.com>
2026-09-18 18:58:43 +0000

    chore(deps): update all dependencies

--- dist/cjs/shim/reactShim.cjs
+++
@@ -1,5 +0,0 @@
-"use strict";
-//#region src/shim/reactShim.ts
-const reactShim = [require("use-sync-external-store/shim/index.js").useSyncExternalStore];
-//#endregion
-exports.reactShim = reactShim;
--- dist/esm/plugin/index.mjs
+++
@@ -1,3 +0,0 @@
-import { applyPlugin } from "./applyPlugin.mjs";
-import { reduxDevtools } from "./reduxDevtools.mjs";
-export { applyPlugin, reduxDevtools };
